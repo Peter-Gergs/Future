87b1d17f806191174a97b04d745619c42bbdbea1
Peter-Gergs <petergergs15@gmail.com>
2025-08-19 12:16:49 +0300

    fix swiper

--- src/modules/SliderItem.jsx
+++ src/modules/SliderItem.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 function SliderItem({ item }) {
   const { t } = useTranslation("home");
 
   const percentage = ((item.discount / item.price) * 100).toFixed(0);
   return (
-    <div className="item">
-      <div className="text">
-        <h3>{item.name}</h3>
-        <h2> {t("discountMessage", { percentage })}</h2>
+    <Link to={`/product/${item.slug}`}>
+      <div className="item">
+        <div className="text">
+          <h3>{item.name}</h3>
+          <h2> {t("discountMessage", { percentage })}</h2>
+        </div>
+        <div className="image">
+          <img src={item.images[0].image} alt="" />
+        </div>
       </div>
-      <div className="image">
-        <img src={item.images[0].image} alt="" />
-      </div>
-    </div>
+    </Link>
   );
 }
 
