4b59d0551e8c04d2c589a468d01ccdcae4ef3a38
Peter-Gergs <petergergs15@gmail.com>
2026-01-25 20:57:25 +0200

    fix arrows

--- src/components/CategorySlider/CategorySlider.jsx
+++ src/components/CategorySlider/CategorySlider.jsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/free-mode";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import "./CategorySlider.css";
 import CategoryItem from "../../modules/CategoryItem";
 import i18n from "../../i18n";
-import API_URL from "../../config";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 function CategorySlider({ sub, title, categories }) {
   const [swiperKey, setSwiperKey] = useState(0);
+
   useEffect(() => {
     setSwiperKey((prev) => prev + 1);
   }, [i18n.language]);
 
   return (
-    <>
-      <div className="sub-word">{sub}</div>
-      <h2 className="title">{title}</h2>
+    <div className="category-slider-wrapper">
+      <div className="slider-header">
+        <div>
+          <div className="sub-word">{sub}</div>
+          <h2 className="title">{title}</h2>
+        </div>
+
+        {/* Custom Arrows */}
+        <div className="slider-arrows">
+          <button className="custom-prev" aria-label="prev">
+            <FaArrowCircleRight />
+          </button>
+          <button className="custom-next" aria-label="next">
+            <FaArrowCircleLeft />
+          </button>
+        </div>
+      </div>
+
       <Swiper
+        key={swiperKey}
         spaceBetween={35}
-        grabCursor={true}
+        grabCursor
         breakpoints={{
           0: { slidesPerView: 2.5 },
           768: { slidesPerView: 3.5 },
@@ -29,13 +46,16 @@ function CategorySlider({ sub, title, categories }) {
           1400: { slidesPerView: 6.5 },
           1600: { slidesPerView: 7.5 },
         }}
-        modules={[Autoplay]}
+        modules={[Autoplay, Navigation]}
+        navigation={{
+          nextEl: ".custom-next",
+          prevEl: ".custom-prev",
+        }}
         autoplay={{
           delay: 4500,
           disableOnInteraction: false,
         }}
         dir={i18n.language === "ar" ? "rtl" : "ltr"}
-        key={swiperKey}
       >
         {categories.map((cat) => (
           <SwiperSlide key={cat.key}>
@@ -47,7 +67,7 @@ function CategorySlider({ sub, title, categories }) {
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   );
 }
 
