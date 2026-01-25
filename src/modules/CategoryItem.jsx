4100f98916f42a83166e7b2648316ff5e4e54b36
Peter-Gergs <petergergs15@gmail.com>
2026-01-25 20:34:17 +0200

    fix width

--- src/modules/CategoryItem.jsx
+++ src/modules/CategoryItem.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function CategoryItem({ name, icon, link }) {
+  console.log(icon);
   return (
     <Link to={link}>
       <div className="category">
-        <image src={icon} alt="name" />
+        <img src={icon} alt={name} style={{ width: "40px",  }} />
         <span>{name}</span>
       </div>
     </Link>
